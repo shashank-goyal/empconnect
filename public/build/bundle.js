617f6797f7d676a0afb3d1111fac2d9bcdc3a9e9
nirmal <nirmaloasis@hotmail.com>
2017-08-30 15:34:45 +0530

    https

--- public/build/bundle.js
+++ public/build/bundle.js
@@ -60977,7 +60977,7 @@ var HomepageLayout = function (_Component) {
                 var request = new XMLHttpRequest();
 
                 var method = 'GET';
-                var url = 'http://maps.googleapis.com/maps/api/geocode/json?latlng=' + latitude + ',' + longitude + '&sensor=true';
+                var url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + latitude + ',' + longitude + '&sensor=true';
                 var async = true;
 
                 request.open(method, url, async);
