c8ccc738b742ccb48c39cc0f7e75cf33c8d98142
Rakesh-Dutta <rakesharya.nitdgp@gmail.com>
2017-09-13 17:38:51 +0530

    updated file path

--- routes/index.js
+++ routes/index.js
@@ -30,27 +30,21 @@ router.post('/insert-classifieds', function(req, res, next) {
 
   const classifieds = connection.get('classifieds');
     classifieds.insert(classifiedsObj,function(err,records){
-      console.log('inside insert ',err,records)
+    console.log('inside insert ',err,records);
+      if (!req.files)
+          return res.status(400).send('No files were uploaded.');
+
+      // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+      let sampleFile = req.files.fileUpload;
+      let list = sampleFile.name.split(".");
+      let ext = list[list.length -1]
+      // Use the mv() method to place the file somewhere on your server
+      sampleFile.mv('public/classifieds/' +records._id +'.' + ext, function(err) {
+          if (err)
+              return res.status(500).send(err);
+          res.send('File uploaded!');
+      });
     })
-        //   if (err) throw err;
-        //   console.log("Record added as " + records[0]._id);
-        //  })
-  console.log(req)
-
-
-    if (!req.files)
-    return res.status(400).send('No files were uploaded.');
-
-  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-  let sampleFile = req.files.fileUpload;
-
-  // Use the mv() method to place the file somewhere on your server
-  sampleFile.mv('public/filename.jpg', function(err) {
-    if (err)
-      return res.status(500).send(err);
-
-    res.send('File uploaded!');
-  });
 
 });
 router.get("/get-classifieds",function(req,res,next){
