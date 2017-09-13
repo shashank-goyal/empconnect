41ce297315885d8563b20f42d8b64f3f1630a8d6
nirmaloasis <tmuni@allstate.com>
2017-09-13 19:12:02 +0530

    achievfgfd

--- views/achievements/AchievementTile.js
+++ views/achievements/AchievementTile.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import SingleAchievementTile from './SingleAchievementTile'
-
+import SingleSportTile from './SingleSportTile'
 export default class AchievementTile extends React.Component{
     
     constructor(props){
@@ -11,37 +11,52 @@ export default class AchievementTile extends React.Component{
             sports:[],
             action:""
         }
+        this.renderTiles = this.renderTiles.bind(this)
 
     }
     componentWillMount(){
-        
-        var allMonths = ['January','February','March', 'April','May','June','July','August','September','October','November','December'];
+      this.renderTiles(this.props);
+    }
+    componentWillReceiveProps(nextProps){
+        this.renderTiles(nextProps)
+    }
+    renderTiles(props){
+       
         var so,sm,sports;
         var update = this.setState.bind(this)
-        if(this.props.name == "home"){
-         so = this.props.data.filter(e => e.type === "Standing Ovation")        
-         sm = this.props.data.filter(e =>e.type==="Star of the month")
-         sports = this.props.data.filter(e =>e.type==="sports")
-         //sorting
-         so = so.sort(function(a,b){
-            return allMonths.indexOf(a.month) > allMonths.indexOf(b.month);
-         });
-         sm = sm.sort(function(a,b){
-            return allMonths.indexOf(a.month) > allMonths.indexOf(b.month);
-         });
-         sports = sports.sort((a,b) => new Date(b.postTimestamp)-new Date(a.postTimestamp))
+        if(props.name == "home"){
+         so = props.data.filter(e => e.type === "Standing Ovation").sort((a,b) => new Date(b.postTimestamp)-new Date(a.postTimestamp)).slice(0,1)        
+         sm = props.data.filter(e =>e.type==="Star of the month").sort((a,b) => new Date(b.postTimestamp)-new Date(a.postTimestamp)).slice(0,1)
+         sports = props.data.filter(e =>e.group==="sports" || e.group==="cultural").sort((a,b) => new Date(b.postTimestamp)-new Date(a.postTimestamp))
+         console.log("sports==>>",sports)
+        
          update({so:so,sm:sm,sports:sports,action:"home"})
         }
-    }
-    
-    render(){
-        
-      return(
-          <div>
-            <SingleAchievementTile data={this.state.so.slice(0,1)}/>
-            <SingleAchievementTile data={this.state.sm.slice(0,1)}/>
+        else{
+            so = props.data.filter(e => e.type === "Standing Ovation" || e.type==="Star of the month").sort((a,b) => new Date(b.postTimestamp)-new Date(a.postTimestamp))        
             
-          </div> 
-        )
+            sports = props.data.filter(e =>e.group==="sports" || e.group==="cultural").sort((a,b) => new Date(b.postTimestamp)-new Date(a.postTimestamp))
+            update({so:so,sm:[],sports:sports,action:""})
+        }
+    }
+
+    render(){
+    
+       if(this.state.action === "home") 
+        return(
+            <div>
+                <SingleAchievementTile data={this.state.so}/>
+                <br/>
+                <SingleAchievementTile data={this.state.sm}/>
+                <br/>
+                <SingleSportTile data={this.state.sports}/>
+            </div> 
+            )
+       else
+        return (
+        <div>
+            {this.state.so.length >0?<SingleAchievementTile data={this.state.so}/>:<SingleSportTile data={this.state.sports}/>}
+        </div> 
+       )     
     }
 }
---
+++ views/achievements/SingleSportTile.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import { Image as ImageComponent, Item,Label,Button,Icon,Segment,Header,Modal } from 'semantic-ui-react'
+
+const imgUrl = {"Table Tennis":"images/table_tennis.jpg","Cricket":"images/cricket_icon.svg","Badminton":"images/badminton.png","Singing":"images/singing.jpg","Dancing":"images/dance.jpg"}
+export default class SingleSportTile extends React.Component{
+  constructor(props){
+      super(props);
+  }
+  
+  
+  render(){
+      var {data} = this.props
+      return(
+          <div>
+          {
+              data.map(e =>    
+              <Segment raised>
+                <Item.Group>
+                    <Item>
+                        <Item.Image size='small'  src={imgUrl[e.type]}/> 
+                        <Item.Content>
+                            <Item.Header>{e.type}</Item.Header>
+                            <Item.Meta>
+                             
+                                <Label as='a' color='green' attached="top right" tag>
+                                    <Icon name='winner' size='large' style={{margin:"0 0.5em 0 0"}}/>{e.subType}
+                                </Label> 
+                               
+                            </Item.Meta> 
+
+                            <Item.Description >
+                                <h4 style={{margin:"0"}}>Winners :-</h4>
+                              <br/>
+                              <div>
+                                <Label id="achievementLink" as='a' image>
+                                <img src='images/gold.svg' />
+                                {e.winners.filter(e => e.rank == "1")[0].name}
+                                
+                                </Label>
+                                <Label id="achievementLink" as='a' image>
+                                <img src='images/silver.svg' />
+                                {e.winners.filter(e => e.rank == "2")[0].name}
+                                
+                                </Label>
+                                <Label id="achievementLink" as='a' image>
+                                <img src='images/bronze.svg' />
+                                {e.winners.filter(e => e.rank == "3")[0].name}
+                                
+                                </Label>
+                            </div>
+                                
+                            </Item.Description> 
+                            <Item.Extra>
+                               <span className='price' style={{float:"left"}}>{e.postTimestamp.split(" ").slice(1,3).reverse().join(" ")}</span>
+                               <Button secondary size='mini' onClick={this.toggleDescription}   floated='right'>Details</Button>
+                            </Item.Extra>
+                        </Item.Content> 
+                    </Item>
+                </Item.Group>  
+                </Segment>   
+              )
+             }
+            </div>
+           
+      )
+  }
+
+}
