11777096d669de9e88e98c014727bd3068ce0958
Rakesh-Dutta <rakesharya.nitdgp@gmail.com>
2017-09-18 20:36:18 +0530

    text changes

--- views/achievements/data.js
+++ views/achievements/data.js
@@ -1,6 +1,8 @@
 const description = [
-    'Cute dogs come in a variety of shapes and sizes. Some cute dogs are cute for their adorable faces, others for their',
-    'tiny stature, and even others for their massive size.',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ' +
+    'ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut' +
+    ' aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat ' +
+    'nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
   ].join(' ')
 const data = [
  { group:"rewards",
